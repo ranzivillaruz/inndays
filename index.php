4a09aac2d550df29fa68b4ba0cb7fff6ccd64f9a
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2025-01-26 21:57:03 +0800

    logo added

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 Index
-(Homepage,carousel preview)
-Register
-Login
+(Homepage,carousel preview) - gio, aj
+Register -erika
+Login -erika
 Footer(contact us)
 Header (logo)
 navbar borgir (faq, lisitngs, profile(login/logout/update))
 FAQ
-Listings(by the beach, mountain,river, lake, most viewed, trending)
 
-10 each with interior
+Listings(by the beach, mountain,river, lake, most viewed, trending) ranzi, erika
+
+
+
+10 each with interior with INFO HA like ilan bedrooms chuchu
 beach- erika
 mountain- aj
 river- daniel
