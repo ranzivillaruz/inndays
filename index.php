975470262cefabc5b2db26690d61e0c68c6cb384
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2025-01-26 21:38:42 +0800

    add server folder and files

--- index.php
+++ index.php
@@ -7,3 +7,9 @@ Header (logo)
 navbar borgir (faq, lisitngs, profile(login/logout/update))
 FAQ
 Listings(by the beach, mountain,river, lake, most viewed, trending)
+
+10 each with interior
+beach- erika
+mountain- aj
+river- daniel
+lake - z
