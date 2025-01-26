c43147ce3dea12a31d681ff3578c9dbb08eaff35
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2025-01-26 21:25:28 +0800

    new files

---
+++ index.php
@@ -0,0 +1,9 @@
+Index
+(Homepage,carousel preview)
+Register
+Login
+Footer(contact us)
+Header (logo)
+navbar borgir (faq, lisitngs, profile(login/logout/update))
+FAQ
+Listings(by the beach, mountain,river, lake, most viewed, trending)
