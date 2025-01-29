8ebdb40bf40752992fe76f4f62b74508cb2ac3d5
AJ <serranoaaronjames028@gmail.com>
2025-01-29 19:10:34 +0800

    Added base design for landing page, finalized header design

--- landing.php
+++ landing.php
@@ -58,7 +58,7 @@
         </div>
 
         <div class="content">
-            <h1>Pakyu <br> <span>Erika</span> </h1>
+            <h1>Pakyu <br> <span>Erika hahahaha ./.</span> </h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
